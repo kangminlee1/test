a9551cf49bb2f1aa933677be976d476a2602f543
kangminlee1 <jack7853@naver.com>
2022-05-01 00:19:03 +0900

    This is Commit test

---
+++ src/test/test1.java
@@ -0,0 +1,10 @@
+package test;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("just test");//Github 업로드 연습용
+	}
+
+}
